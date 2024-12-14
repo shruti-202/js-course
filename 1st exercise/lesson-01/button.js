aecf54d6147283423eb771115f51f965ecb35301
Shruti <shrutigawande1910@gmail.com>
2024-12-14 13:12:28 +0530

    first lesson

---
+++ 1st exercise/lesson-01/button.js
@@ -0,0 +1,10 @@
+var btnAdd = document.querySelector('.btn-add');
+btnAdd.addEventListener('click', function clickEventHandler(){
+    alert("clicked")
+})
+
+var btnBuy = document.querySelector('.btn-buy');
+btnBuy.addEventListener('click', function buyHandler(){
+    console.log('displaying....');
+    alert("purchased")
+})
